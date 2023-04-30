84210c141385039a9f780fe4b21d4d0f2b62f934
Vovashprot <vova.protasenko@gmail.com>
2023-04-30 18:57:43 +0300

    2

--- demo/src/main/java/think/example/demo/EmployeeService.java
+++ demo/src/main/java/think/example/demo/EmployeeService.java
@@ -1,27 +1,40 @@
 
 package think.example.demo;
 
+import exceptions.EmployeeAlreadyAddedException;
 import exceptions.EmployeeNotFoundException;
+import exceptions.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 @Service
 public class EmployeeService {
+    int listLimit = 5;
     List<Employee> employees = new ArrayList<>();
     public String addEmployee(String firstName, String lastName){
+        if(employees.size() >= listLimit){
+            throw new EmployeeStorageIsFullException();
+        }
         Employee newEmployee = new Employee(firstName, lastName);
+        if (employees.contains(newEmployee)){
+            throw new EmployeeAlreadyAddedException();
+        }
         employees.add(newEmployee);
         return "Employee added";
     }
-    public String searchEmployee(String firstName, String lastName){
+    public Employee searchEmployee(String firstName, String lastName){
         Employee searchedEmployee = new Employee(firstName, lastName);
       for (int i = 0; i<employees.size();i++)  {
         if  (employees.get(i).equals(searchedEmployee)) {
-            return employees.get(i).toString();
+            return employees.get(i);
         }
       }
       throw new EmployeeNotFoundException();
+    }
+    public String deleteEmployee(String firstName, String lastName){
+        employees.remove(searchEmployee(firstName,lastName));
+        return "Employee deleted";
     }
     public List<Employee> getEmployees() {
         return employees;
